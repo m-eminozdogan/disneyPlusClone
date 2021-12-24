a61ded63292424380509f8c027447e3a8fec5760
M Emin Özdoğan <m.eminozdogan@hotmail.com>
2021-12-24 20:48:26 +0300

    added: google login/logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,117 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
+import { useNavigate } from 'react-router-dom'
+import { selectUserName, selectUserPhoto, setSignOut, setUserLogin } from '../features/user/userSlice'
+import { useSelector, useDispatch } from 'react-redux'
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+
 function Header() {
+    const dispatch = useDispatch()
+    const userName = useSelector(selectUserName);
+    const userPhoto = useSelector(selectUserPhoto);
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
+    const navigate = useNavigate()
+    useEffect(() => {
+        auth.onAuthStateChanged(async (user)=>{
+            if(user){
+                dispatch(setUserLogin({
+                    name: user.displayName,
+                    email: user.email,
+                    photo: user.photoURL
+                }))
+                navigate('/')
+            }
+        })
+    }, [])
+
+
+
+    const signIn = () => {
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                //const credential = GoogleAuthProvider.credentialFromResult(result);
+                //const token = credential.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                //console.log(user);
+                dispatch(setUserLogin({
+                    name: user.displayName,
+                    email: user.email,
+                    photo: user.photoURL
+                }))
+                navigate('/')
+            }).catch((error) => {
+                // Handle Errors here.
+                //const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                //const email = error.email;
+                // The AuthCredential type that was used.
+                //const credential = GoogleAuthProvider.credentialFromError(error);
+                // ...
+                console.log(errorMessage)
+            });
+    }
+
+    function handleSignOut() {
+        signOut(auth).then(() => {
+            // Sign-out successful.
+            dispatch(setSignOut())
+            navigate('/login')
+        }).catch((error) => {
+            // An error happened.
+            alert('çıkış yapılırken hata')
+        });
+    }
+
     return (
         <div>
             <Nav>
-                <Logo src='/images/logo.svg'/>
-                <NavMenu>
-                    <a href="/">
-                        <img src='/images/home-icon.svg' alt=""/>
-                        <span>HOME</span>
-                    </a>
-                    <a href="#">
-                        <img src='/images/search-icon.svg' alt=""/>
-                        <span>SEARCH</span>
-                    </a>
-                    <a href='#'>
-                        <img src='/images/watchlist-icon.svg' alt=""/>
-                        <span>WATCHLIST</span>
-                    </a>
-                    <a href='#'>
-                        <img src='/images/original-icon.svg' alt=""/>
-                        <span>ORIGINALS</span>
-                    </a>
-                    <a href='#'>
-                        <img src='/images/movie-icon.svg' alt=""/>
-                        <span>MOVIES</span>
-                    </a>
-                    <a href='#'>
-                        <img src='/images/series-icon.svg' alt=""/>
-                        <span>SERIES</span>
-                    </a>
-                </NavMenu>
-                <UserImg alt="" src="https://scontent.fist2-3.fna.fbcdn.net/v/t1.6435-1/c0.25.200.200a/p200x200/48035070_2266390030272527_2164551719185285120_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=7206a8&_nc_ohc=a_5pKjldDcwAX8JZS5y&_nc_ht=scontent.fist2-3.fna&oh=00_AT_j2o8MAARWXndYG7uiAM4Nzw5ojKPVr1cADaJvnHMytA&oe=61E1F4C3" />
+                <Logo src='/images/logo.svg' />
+                {!userName ? (
+                    <LoginContainer>
+                        <Login onClick={signIn}>
+                            LOGIN
+                        </Login>
+                    </LoginContainer>
+                ) : (
+                    <>
+                        <NavMenu>
+                            <a href="/">
+                                <img src='/images/home-icon.svg' alt="" />
+                                <span>HOME</span>
+                            </a>
+                            <a href="#">
+                                <img src='/images/search-icon.svg' alt="" />
+                                <span>SEARCH</span>
+                            </a>
+                            <a href='#'>
+                                <img src='/images/watchlist-icon.svg' alt="" />
+                                <span>WATCHLIST</span>
+                            </a>
+                            <a href='#'>
+                                <img src='/images/original-icon.svg' alt="" />
+                                <span>ORIGINALS</span>
+                            </a>
+                            <a href='#'>
+                                <img src='/images/movie-icon.svg' alt="" />
+                                <span>MOVIES</span>
+                            </a>
+                            <a href='#'>
+                                <img src='/images/series-icon.svg' alt="" />
+                                <span>SERIES</span>
+                            </a>
+                        </NavMenu>
+                        <UserImg onClick={handleSignOut} alt="" src={userPhoto} />
+
+                        {/*<UserImg onClick={handleSignOut} alt="" src="https://scontent.fist2-3.fna.fbcdn.net/v/t1.6435-1/c0.25.200.200a/p200x200/48035070_2266390030272527_2164551719185285120_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=7206a8&_nc_ohc=a_5pKjldDcwAX8JZS5y&_nc_ht=scontent.fist2-3.fna&oh=00_AT_j2o8MAARWXndYG7uiAM4Nzw5ojKPVr1cADaJvnHMytA&oe=61E1F4C3" />*/}
+
+                    </>)
+
+                }
+
             </Nav>
         </div>
     )
@@ -39,7 +119,7 @@ function Header() {
 
 export default Header
 
-const Nav =styled.nav`
+const Nav = styled.nav`
     overflow-x:hidden;
     height:70px;
     background:#090b13;
@@ -48,11 +128,30 @@ const Nav =styled.nav`
     padding: 0 36px;    
 
 `
-const Logo=styled.img`
+const Logo = styled.img`
     width:80px;
     
 `
-const NavMenu=styled.div`
+const LoginContainer = styled.div`
+    flex:1;
+    display:flex;
+    justify-content:flex-end;
+`
+const Login = styled.div`
+    cursor:pointer;
+    border:1px solid #f9f9f9;
+    border-radius:4px;
+    padding:8px 16px;
+    letter-spacing:1.5px;
+    background-color:rgba(0,0,0, 0.6);
+    transition:all 0.2s ease 0s;
+    &:hover{
+        background-color:#f9f9f9;
+        color:#000;
+        border-color:transparent;
+    }
+`
+const NavMenu = styled.div`
     display:flex;
     flex:1;
     margin-left:25px;
@@ -90,7 +189,7 @@ const NavMenu=styled.div`
        }
     }
 `
-const UserImg=styled.img`
+const UserImg = styled.img`
     width:48px;
     heigth:48px;
     border-radius:50%;
